dd8b648b5a4b6bd5ba7e69731acf04fe78afd894
Andrespineda021 <157128206+Andrespineda021@users.noreply.github.com>
2024-04-12 08:13:34 -0600

    Add files via upload

---
+++ semana 13/laboratorio/Untitled-1.py
@@ -0,0 +1,24 @@
+# # print("CICLO WHILE")
+# # i=1
+# # while i <=10
+# #   print("2x"+str(i)+"=")
+# # i +=1
+
+# print("CICLO FOR")
+
+# for X in range(1,5):
+#     print(X)
+
+# numero = int(input("ingrese un numero:"))
+
+# print(f"tabla de multiplicar del {numero}:")
+# for i in range (1,11):
+#     resultado = numero * i
+#     print(f"{numero} x {i} = {resultado}")
+
+ #  programa numero 3
+numero = int(input("igrese numero:"))
+suma=0
+for i in range(1, numero +1):
+    suma += 1
+    print(f" la suma dsde 1 hasta {numero} es: {suma}")
